03beee032451a6f4fde3a8c6e2675843d0c8cce0
thushan-123 <thushanmadusanka456@gmail.com>
2025-07-31 21:53:39 +0530

    add peek func

--- stack/stack.c
+++ stack/stack.c
@@ -4,6 +4,28 @@
 
 // STACK work LIFO (last in first out)
 
+// Operations
+// push -> insert data in stack
+// peek -> return to the peek value
+// pop  -> remove data in stack
+// isEmpty -> returns stack emty or not
+
+// struct key word access . operator for variable , -> operator for pointor 
+typedef struc{
+    int data;
+    int *next_node
+}   Node;
+
+Node *head;
+
+
+
+int peek(){
+    return 0;
+}
+
+
+
 
 int main(){
     return 0;
