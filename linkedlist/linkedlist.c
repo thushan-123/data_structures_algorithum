373178b08e5a901ca0c39a21994d5fdabf4aaab4
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-01 16:12:05 +0530

    create a data tye node

---
+++ linkedlist/linkedlist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node * next;
+}
+
+// first we init the LinkedList
+
+// void linedList(){
+
+// }
+
+
+int main(){
+
+
+
+
+    return 0;
+}
