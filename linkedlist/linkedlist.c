30ceda9c4ec84284c58c4f5af08303bd20a7bd1a
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-02 16:14:36 +0530

    test ledlst

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -16,7 +16,7 @@ struct linkedlist{
     
 };
 
-void linedList(struct linkedlist *name){
+void linedList_init(struct linkedlist *name){
     name->head = NULL;
     name->temp = NULL;
 }
@@ -34,7 +34,7 @@ void add(struct linkedlist *lnklst,int value){
     }
     newnode->data = value;
 
-    if(head == NULL){
+    if(lnklst->head == NULL){
         // add first element to linked list
         lnklst->head = newnode;
         lnklst->temp = newnode;
@@ -46,7 +46,7 @@ void add(struct linkedlist *lnklst,int value){
 
 void print(struct linkedlist *lnklst){
     if(lnklst->head == NULL){
-        printf("No Elements In LinkedList");
+        printf("\nNo Elements In LinkedList\n");
     }else{
         struct node * t = lnklst->temp;
         while(t != NULL){
@@ -59,11 +59,23 @@ void print(struct linkedlist *lnklst){
 
 int main(){
 
-    add(10);
-    add(20);
-    add(30);
+    struct linkedlist l;
+    linedList_init(&l);
+    print(&l);
+    add(&l,1);
+    add(&l,2);
+    add(&l,3);
+    add(&l,4);
+    print(&l);
 
-    print();
+    struct linkedlist k;
+    linedList_init(&k);
+    print(&k);
+    add(&k,10);
+    add(&k,20);
+    add(&k,30);
+    add(&k,40);
+    print(&k);
 
 
     return 0;
