54b644880b93e143a28e5eff3a0d21066551334f
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-06 21:02:39 +0530

    create data types

--- queue/queue_2stack.c
+++ queue/queue_2stack.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct stack{
+    int count;
+    struct node *temo;
+    struct node *head;
+};
+
+
+
 
 int main(){
 
