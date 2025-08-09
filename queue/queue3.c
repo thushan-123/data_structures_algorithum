36eb625779c68c8021001e708d006950255cfc62
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-09 22:17:38 +0530

    intilized data types

--- queue/queue3.c
+++ queue/queue3.c
@@ -3,10 +3,23 @@
 #include <stdbool.h>
 
 
+struct node{
+    int data;
+    struct node *next;
+}
+
+struct queue{
+    int count;
+    struct node head;
+    struct node temp;
+}
+
+
+
 
 
 int main(){
 
     return 0;
-    
+
 }
