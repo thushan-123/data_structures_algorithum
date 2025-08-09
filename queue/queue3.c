8182ede1ca760e153d0f279064e51c7a73058ac6
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-09 22:39:59 +0530

    fix pointer errs

--- queue/queue3.c
+++ queue/queue3.c
@@ -6,25 +6,47 @@
 struct node{
     int data;
     struct node *next;
-}
+};
 
 struct queue{
     int count;
-    struct node head;
-    struct node temp;
-}
+    struct node *head;
+    struct node *tail;
+};
 
 void init_queue(struct queue *q){
     q->count = 0;
     q->head = NULL;
-    q->temp = NULL;
+    q->tail = NULL;
 }
 
+bool enqueue(struct queue *q, int value){
+
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+
+    if(new_node == NULL){
+        printf("Memory Allocation Fail");
+        exit(1);
+    }
+    new_node->data = value;
+    q->count++;
+
+    if(q->head == NULL){
+        q->head = new_node;
+        q->tail = new_node;
+    }else {
+        q->tail->next = new_node;
+    }
+}
 
 
 
 int main(){
 
+    struct queue q;
+    init_queue(&q);
+    enqueue(&q,10);
+    enqueue(&q,20);
     return 0;
 
 }
