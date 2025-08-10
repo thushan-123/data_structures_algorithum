e8ecde007d0a922aff0e7fdcc3c993a292a74b18
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-10 17:12:04 +0530

    create init q fun

--- queue/queue_2stack.c
+++ queue/queue_2stack.c
@@ -9,20 +9,36 @@ struct node {
 
 struct stack{
     int count;
-    struct node *temo;
+    struct node *temp;
     struct node *head;
 };
 
 
 struct queue{
+    int queue_count;
     struct stack *stack1;
     struct stack * stack2;
 }
 
+// first create stack methods;
+
+// impemnt push method
+
+
+
 
 
 void init_queue(struct queue* q){
+    q->stack1->head = NULL;
+    q->stack1->temp = NULL;
+    q->stack1->count = 0;
     
+    q->stack2->head = NULL;
+    q->stack2->temp = NULL;
+    q->stack2->count = 0;
+
+    q->queue_count = 0;
+
 }
 
 
