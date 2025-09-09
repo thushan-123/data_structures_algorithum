19e4ec64c7f419d02ae29e4590c859c471e976c0
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-09 20:52:27 +0530

    bug fix

--- string/compare_string.c
+++ string/compare_string.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 
-int compare_string(char string1[], char string2[]){
+int compare_string(char *string1, char *string2){
 
     int f =1;
 
     int len1 = sizeof(string1)/sizeof(string1[0]);
     int len2 = sizeof(string2)/sizeof(string2[0]);
+    printf("%i %i", len1, len2);
     if(len1 == len2){
 
-        for(int i=0; string1[i] !='\0', i++){
+        for(int i=0; string1[i] !='\0' || string2[i] !='\0' ; i++){
             if(string1[i] != string2[i]){
-                continue;
-            }else{
                 f =0;
                 break;
             }
@@ -29,6 +28,10 @@ int compare_string(char string1[], char string2[]){
 
 int main() {
 
+    char s1[] = "hello";
+    char s2[] = "hello";
+
+    printf(" compare %i \n", compare_string(s1,s2));
 
     return 0;
 }
