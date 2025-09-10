595e35c5237a8a4c8420198b62b5d5b2d80e36aa
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-10 18:25:39 +0530

    rm sizeof add len_str fn

--- string/concat_string.c
+++ string/concat_string.c
@@ -12,11 +12,11 @@ int length_string(char *str){
 
 char* concat_string(char *string1, char *string2){
 
-    int len1 = sizeof(string1)/sizeof(string1[0]);
-    int len2 = sizeof(string2)/sizeof(string2[0]);
-    int size = len1 + len2;
+    int len1 = length_string(string1);
+    int len2 = length_string(string2);
+    int size = len1 + len2 -1;
 
-    char *string3=malloc(len1+len2-1);
+    char *string3=malloc(size);
 
     for(int k=0; string1[k] != '\0'; k++){
         string3[k] = string1[k];
