ddc6cd16d203b0023653ee455ef122089c88efbd
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-11 18:31:26 +0530

    add sring length fn

--- string/copy_string.c
+++ string/copy_string.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int length_string(char * string){
+    int k = 0;
+
+    for (int i=0; string['\0']; i++){
+        k +=1;
+    }
+
+    return k;
+}
 
 
 int main() {
